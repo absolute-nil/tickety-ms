4d4b84853589885a2a2d843e1812ac1533daf205
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-06 18:16:53 +0530

    connect routes and middlewares

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,37 @@
 import express from "express";
 
+//route handlers
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/sign-in";
+import { signUpRouter } from "./routes/sign-up";
+import { signOutRouter } from "./routes/sign-out";
+
+// error
+import { NotFoundError } from "./errors/not-found-error";
+
+//middleware
+import { errorHandler } from "./middlewares/error-handler";
+
+
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000,()=>{
+//routes
+app.use("/api/users/currentuser", currentUserRouter);
+app.use("/api/users/signup", signUpRouter);
+app.use("/api/users/signin", signInRouter);
+app.use("/api/users/signout", signOutRouter);
+
+// if route does not exist
+app.get("*", () => {
+  throw new NotFoundError();
+
+})
+
+// middleware
+app.use(errorHandler);
+
+app.listen(3000, () => {
   console.log("auth service running on port 3000");
 })
