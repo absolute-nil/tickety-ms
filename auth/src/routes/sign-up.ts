ec0801d6ec18e60ce19fb8b74ae191db3283e3e1
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-08 22:32:09 +0530

    create jwt and set cookie on signup

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from "express-validator";
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from "../errors/database-connection-error"
+import jwt from 'jsonwebtoken'
 
+import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from "../models/user";
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -14,16 +16,37 @@ router.post("/", [
     .trim()
     .isLength({ min: 4, max: 20 })
     .withMessage("password must be between 4 and 20 character long"),
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array());
   }
   const { email, password } = req.body;
-  console.log("creating a user..../");
-  throw new DatabaseConnectionError();
 
-  res.send({});
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new BadRequestError('Email already in use');
+  }
+
+  const user = User.build({
+    email,
+    password
+  })
+  await user.save();
+
+  //generate jwt
+  const userJwt = jwt.sign({
+    id: user._id,
+    email: user.email
+  }, 'asfd');
+
+  // store it on session
+  req.session = {
+    jwt: userJwt,
+  };
+
+  res.status(201).send(user);
 
 })
 
