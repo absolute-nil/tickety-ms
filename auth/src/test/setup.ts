4188bc43674d8ebe06aabdfaf810ce389df28e7f
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-10 21:55:24 +0530

    implement a global signin function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,17 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
+
 import { app } from "../app";
+
+// add signin property to global
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 let mongo: any;
@@ -30,3 +41,21 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 })
+
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+}
