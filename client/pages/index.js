0ebeafe9c96a31ab9973c5cb3917faad9826d3c0
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-11 17:41:03 +0530

    succesfully route from server and client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,34 @@
-const Index = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Hello world</h1>;
 };
 
-export default Index;
+LandingPage.getInitialProps = async ({ req }) => {
+  // cant use useRequest because hooks can only be used inside react components and getInitialProps
+  // is a function
+  if (typeof window === 'undefined') {
+    // we are on server
+    // request should be mate to ingress server
+    // ! since this request is being made from server the cookies and all are not included so add them manually
+    //* http://SERVICENAME.NAMESPACE.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        // need to specify the host as there may be many different rules related to different domains in ingress
+        // req.headers includes cookies Host and other things relevant to make request
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // we are on browser we can just use the default tickety.com by browser
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+};
+
+export default LandingPage;
