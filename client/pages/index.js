7f1f963ec0c0299aeb9156db30a248e5a4d4ac94
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-11 19:29:16 +0530

    remove axios logic from index to build-client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,34 +1,19 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  return <h1>Hello world</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  // cant use useRequest because hooks can only be used inside react components and getInitialProps
-  // is a function
-  if (typeof window === 'undefined') {
-    // we are on server
-    // request should be mate to ingress server
-    // ! since this request is being made from server the cookies and all are not included so add them manually
-    //* http://SERVICENAME.NAMESPACE.svc.cluster.local
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        // need to specify the host as there may be many different rules related to different domains in ingress
-        // req.headers includes cookies Host and other things relevant to make request
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    // we are on browser we can just use the default tickety.com by browser
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
+  return data;
 };
 
 export default LandingPage;
