de7a4f712d00209746ee6c65255ffc140179ce9c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-11 19:30:58 +0530

    implement gip and add gloabal header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/Header';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+// the Component, pageProps and currentUser we get from getInitialProps
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  // to execute the getInitialProps of the pages we have to implement this
+  // this happens because we defined the get initial props in _app so the page's getInitialProps
+  // wont get invoked automatically
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
