c6bcf97bc0dc2060d0874d856cf08e6512a204bc
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-13 17:30:16 +0530

    implement options to recieve missed events

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -17,12 +17,18 @@ stan.on('connect', () => {
   })
   const options = stan
     .subscriptionOptions()
-    // manual acknowledgement to true so that if while processing the event something goes wrong we can follow up again and event will not be lost;
-    .setManualAckMode(true);
+    // manual acknowledgement to true so that if while processing the event something goes wrong 
+    //we can follow up again and event will not be lost;
+    .setManualAckMode(true)
+    // send all events that were there after it starts up (only the first time it starts up if we use set durable name)
+    .setDeliverAllAvailable()
+    // send only those events which were not processed by this service 
+    //(it will only work if we have a queue group name otherwise it will delete the durable name and it will keep sending all the events in history)
+    .setDurableName('orders-service');
 
   const subscription = stan.subscribe('ticket:created',
-    // you need to specify the below string for creating a group for services (in case of horizontal scaling)
-    'orders-service-queue-group',
+    // you need to specify the below string for creating a group for services (in case of horizontal scaling) (queue group name)
+    'queue-group-name',
     options);
 
   subscription.on('message', (msg: Message) => {
@@ -35,7 +41,7 @@ stan.on('connect', () => {
     msg.ack()
   })
 })
-
+// for nodemon SIGUSR2
 // this executes stan.close just after we hit ctrl + c or rs and then closes the window
 process.on('SIGINT', () => stan.close())
 process.on('SIGTERM', () => stan.close())
