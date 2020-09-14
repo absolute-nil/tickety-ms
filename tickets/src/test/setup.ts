8bf4e567d1ffb891632bdceb2f9c43243e63df31
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-14 23:19:45 +0530

    add nats-wrapper mock

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -14,6 +14,9 @@ declare global {
   }
 }
 
+// mock the nats wrapper (whereever nats-wrapper.ts will be imported it will redirect to the fake file in mocks)
+jest.mock('../nats-wrapper.ts')
+
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 let mongo: any;
 beforeAll(async () => {
@@ -30,6 +33,9 @@ beforeAll(async () => {
 
 
 beforeEach(async () => {
+  // clear the mock data (this will clear the data in mocks so that if one test invoked the nats wrapper)
+  // and the other one does not we can capture that (it will basically change the flag of invoked to false before each test)
+  jest.clearAllMocks();
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
