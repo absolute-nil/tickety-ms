6f15f682d6b79beb7cd8d4ca7df9ca45e58cdf18
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-14 21:39:08 +0530

    implement ticket created publisher

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@n19tickety/common";
+
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 
 const router = express.Router();
@@ -24,6 +27,12 @@ router.post('/', requireAuth, [
 
   await ticket.save()
 
+  await new TicketCreatedPublisher(natsWrapper.client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId
+  })
   res.status(201).send(ticket);
 })
 
