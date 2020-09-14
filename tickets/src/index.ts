98a423e68587e1ea5562e50859efb983f6299c1c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-14 23:20:03 +0530

    use nats env vars

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,14 +5,25 @@ import { natsWrapper } from "./nats-wrapper";
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
-
   }
+
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
+  }
 
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
   }
   try {
-    await natsWrapper.connect('tickety', 'rgferf', 'http://nats-srv:4222')
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
     natsWrapper.client.on('close', () => {
       // to immediately tell nats to close the connection when it goes offline (so that there is no delay in events)
       console.log('NATS connection closed');
