36f8fda2dfc41aa1a97a9650429c7fb6dbf26828
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-17 22:58:48 +0530

    implement index router

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,11 +1,14 @@
+import { requireAuth } from "@n19tickety/common";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 
 const router = express.Router();
 
 
-router.get("/", async (req: Request, res: Response) => {
-  res.send({})
+router.get("/", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({ userId: req.currentUser!.id }).populate('ticket');
+  res.send(orders);
 })
 
 export { router as indexOrderRouter };
