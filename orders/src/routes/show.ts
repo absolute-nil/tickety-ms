8c08547dff43e616a9c7540e776c9ee9f2b7acf5
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-17 22:59:03 +0530

    implement show router

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from "express";
+import { requireAuth, NotFoundError, NotAuthorizedError } from "@n19tickety/common";
 
+import { Order } from "../models/order";
 
 const router = express.Router();
 
+// @url: /api/orders/:orderId
+// method: GET
+// returns an order
+router.get("/", requireAuth, async (req: Request, res: Response) => {
+  const orderId = req.params.orderId;
+  const order = await Order.findById(orderId).populate('ticket');
 
-router.get("/", async (req: Request, res: Response) => {
-  res.send({})
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  res.send(order);
 })
 
 export { router as showOrderRouter };
