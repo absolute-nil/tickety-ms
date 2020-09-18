e0403ff7dbb30b9f4128270eaf81801571109c5d
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-18 14:49:34 +0530

    create tests for delete ticket router

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,64 @@
+import request from "supertest"
+import mongoose from "mongoose"
+
+import { app } from "../../app"
+import { Order, OrderStatus } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
+
+
+it('marks an order as cancelled', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 200
+  });
+
+  await ticket.save()
+
+  const user = global.signin();
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  // make sure order is cancelled
+  const cancelledOrder = await Order.findById(order.id)
+  expect(cancelledOrder!.status).toEqual(OrderStatus.Cancelled);
+
+})
+
+it('emits an order cancelled event ', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 200
+  });
+
+  await ticket.save()
+
+  const user = global.signin();
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+})
