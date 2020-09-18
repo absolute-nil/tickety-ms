26c507035a5161a425effec55188edee69cb317b
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-18 14:50:09 +0530

    create tests for new order router

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose"
 import { app } from "../../app"
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it('returns an error if ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -58,4 +59,19 @@ it('reserves a ticket', async () => {
 
 })
 
-it.todo('emits an order created event');
+it('emits an order created event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 2000
+  });
+
+  await ticket.save()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
