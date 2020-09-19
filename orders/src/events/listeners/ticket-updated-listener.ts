b4fdc0c8eee16c5388bb23a36d865e0069ff69af
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-19 20:44:02 +0530

    implementation of Max retires and pre save

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -4,27 +4,34 @@ import { Ticket } from "../../models/ticket";
 import { queueGroupName } from "./queue-group-name";
 
 
-export class TicketUpdatedListner extends Listener<TicketUpdatedEvent> {
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-    const ticket = await Ticket.findById(data.id);
+    const ticket = await Ticket.findByIdAndVersion(data);
 
     if (!ticket) {
+      //* if the ticket gets rejected more than MAX_RETIRES times then it will
+      //* be acknowledged and will be discarded
+      // @ts-ignore
+      // if (msg.getRedeliveryCount() > process.env.MAX_RETIRES) {
+      //   msg.ack();
+      // }
       throw new Error('Ticket not found');
     }
 
+    //!while using mongoose-update if current
     const { title, price } = data;
     ticket.set({ title, price });
-    await ticket.save();
 
+    //!while not using mongoose-update if current
+    // const { title, price, version } = data;
+    // ticket.set({ title, price, version });
+
+    await ticket.save();
 
     msg.ack();
   }
 }
 
-    // // @ts-ignore
-    // if(msg.getRedeliveryCount() > process.env.MAX_RETIRES){
-
-    // }
