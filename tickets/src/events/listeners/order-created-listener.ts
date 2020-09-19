c6df7ee831f86e6fbf6ec6dafef6037a1f1f4b4e
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-19 20:47:27 +0530

    create order created listener

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,41 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@n19tickety/common";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving 
+    const ticket = await Ticket.findById(data.ticket.id)
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    // ! this will change the version so you need to emit
+    // ! an event to give other services the updated version
+    await ticket.save();
+
+    // tell other services that version changed
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version
+    });
+
+    // ack the message
+    msg.ack();
+  }
+}
