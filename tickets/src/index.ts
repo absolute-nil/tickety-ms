1938c9e738ea61ee54b174982855de7242ff4b3d
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2020-09-19 20:46:56 +0530

    implement order can celled and created listeners

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 import { app } from './app'
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -35,6 +37,8 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close())
     // process.on('SIGUSR2', () => natsWrapper.client.close())
 
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
